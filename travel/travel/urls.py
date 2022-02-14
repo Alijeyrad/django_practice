ed71bf72eb29f46ddc7869e5aa016930f08f7472
Alijeyrad <89392975+Alijeyrad@users.noreply.github.com>
2022-02-14 21:54:51 +0330

    fixed banners in layout

--- travel/travel/urls.py
+++ travel/travel/urls.py
@@ -22,7 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('home/', include('home.urls')),
     path('blog/', include('blog.urls'))
 ]
 
