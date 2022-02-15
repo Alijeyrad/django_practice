61445d3eeb9a753a493fa398e8e7dd0825d85fef
Alijeyrad <89392975+Alijeyrad@users.noreply.github.com>
2022-02-15 17:51:04 +0330

    made unpublished posts inaccessible
    this commit contains a security trick!

--- travel/blog/views.py
+++ travel/blog/views.py
@@ -10,6 +10,7 @@ def blog_home(request):
     return render(request, 'blog/blog-home.html', context)
 
 def blog_single(request, pid):
-    post = get_object_or_404(Post, id=pid)
+    post = Post.objects.filter(status=1)
+    post = get_object_or_404(post, id=pid)
     context = {'post':post}
     return render(request, 'blog/blog-single.html', context)
