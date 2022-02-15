5da8aa6dde2b59e1e1de1986ceb8f8925693c757
Alijeyrad <89392975+Alijeyrad@users.noreply.github.com>
2022-02-15 15:06:01 +0330

    added posts from db to the blog

--- travel/blog/views.py
+++ travel/blog/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from home.models import Post
 
 # Create your views here.
 
 def blog_home(request):
-    return render(request, 'blog/blog-home.html')
+    posts = Post.objects.filter(status = 1)
+    context = {'posts':posts}
+    return render(request, 'blog/blog-home.html', context)
 
 def blog_single(request):
     return render(request, 'blog/blog-single.html')
