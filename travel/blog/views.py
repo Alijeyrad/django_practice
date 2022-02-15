00e63b6866dea47d3274ba8a7ba383451a7e0f82
Alijeyrad <89392975+Alijeyrad@users.noreply.github.com>
2022-02-15 15:35:46 +0330

    added chained filters for posts

--- travel/blog/views.py
+++ travel/blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from home.models import Post
+import datetime
 
 # Create your views here.
 
 def blog_home(request):
-    posts = Post.objects.filter(status = 1)
+    posts = Post.objects.filter(status = 1).filter(published_date__lte=datetime.datetime.now())
     context = {'posts':posts}
     return render(request, 'blog/blog-home.html', context)
 
