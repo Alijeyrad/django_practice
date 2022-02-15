94e6812403d4ebdc3a27dfa678ba7bdd444d7c28
Alijeyrad <89392975+Alijeyrad@users.noreply.github.com>
2022-02-15 12:55:02 +0330

    post and contact models created for home app

--- travel/home/admin.py
+++ travel/home/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Post, Contact
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ['title', 'counted_views', 'status', 'created_date']
+    list_filter = ('status',)
+    search_fields = ['title','content']
+    #ordering = ('-created_date',)
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    pass
--- travel/home/models.py
+++ travel/home/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    # image =
+    #author =
+    # tags = 
+    # category = 
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.id} - {self.title}"
+
+    class Meta:
+        ordering = ['-created_date']
+
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=255)
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
