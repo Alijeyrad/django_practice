0f3aaf28d3a864ca41622026dcf37a3fc89505e6
Alijeyrad <89392975+Alijeyrad@users.noreply.github.com>
2022-02-16 16:29:40 +0330

    added 'next' and 'prev' posts to single post blog page

--- travel/blog/views.py
+++ travel/blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from home.models import Post
 import datetime
 
+# My functions
+
 # Create your views here.
 
 def blog_home(request):
@@ -10,7 +12,20 @@ def blog_home(request):
     return render(request, 'blog/blog-home.html', context)
 
 def blog_single(request, pid):
-    post = Post.objects.filter(status=1)
-    post = get_object_or_404(post, id=pid)
-    context = {'post':post}
+    all_posts = Post.objects.filter(status=1).order_by('id')
+    post = get_object_or_404(all_posts, id=pid)
+    post_index = list(all_posts).index(post)
+    try:
+        next_post = [list(all_posts)[post_index+1]]
+    except:
+        next_post = []
+    try:
+        prev_post = [list(all_posts)[post_index-1]]
+    except:
+        prev_post = []
+    context = {
+        'post':post,
+        'next_post':False if len(next_post) == 0 else next_post[0],
+        'prev_post':False if len(prev_post) == 0 else prev_post[0]
+    }
     return render(request, 'blog/blog-single.html', context)
